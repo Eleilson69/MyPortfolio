b728d6408111e1091251b669ab0479fe034ae1ea
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-10-24 14:02:57 -0300

    update

--- js/script.js
+++ js/script.js
@@ -18,6 +18,24 @@ const dotcard = document.querySelector('.dot-card');
 const dotcard2 = document.querySelector('.dot-card-2');
 
 
+
+
+const handleScrollToPageBottom = () => {
+const header = document.querySelector('header');
+    const { scrollTop } = document.documentElement;
+    if (!scrollTop) {
+        header.style.boxShadow = '0px 0px 0px black'
+        header.style.background = ''
+        header.style.transition = '0.3s ease-in-out'
+        return
+    }
+    header.style.boxShadow = '0px 0px 10px black'
+    header.style.background = '#130725'
+    header.style.transition = '0.3s ease-in-out'
+};
+
+window.addEventListener('scroll', () => handleScrollToPageBottom())
+
 const allLi = document.querySelectorAll('.rds-conteiner');
 
 const amPm = () => {
@@ -82,57 +100,56 @@ allLi.forEach((li, index) => {
     const dotPai = document.querySelector('.dotPai');
 
     li.addEventListener('mouseenter', (e) => {
-        if(index === 0){
-            dotPai.style.transform = `translateX(-15px)`;
-            dotcard2.innerHTML = `
-            <p>
-                Oi! me chama no <strong style="color: rgb(0, 255, 102);">Whatsapp</strong>
-                vamos converssar.
-            </p>`;
-        };
-        if(index === 1){
-            dotPai.style.transform = `translateX(93.5px)`;
-            dotcard2.innerHTML = `
-            <p>
-                Me segue no 
-                <strong style="color: rgb(255, 0, 128);">Instagram</strong>
-                e me chama no direct.
-            </p>`;
-        }
-        if(index === 2){
-            dotPai.style.transform = `translateX(203px)`;
-            dotcard2.innerHTML = `
-            <p>
-                Passa lá no meu <strong style="color:  rgb(39, 48, 161);">Linkedin</strong>.
-            </p>`;
-        };
-        if(index === 3){
-            dotPai.style.transform = `translateX(311px)`;
-            dotcard2.innerHTML = `
-            <p>
-                Dá uma olhadinha lá no meu 
-                <strong 
-                    style="
-                        color: black; 
-                        text-shadow:  1px 1px 0px #F7004E, -1px -1px 0px  #00EAE3;
-                    ">
-                    TikTok
-                </strong>.
-            </p>
-            `;
-        };
-        if(index === 4){
-            dotPai.style.transform = `translateX(420px)`;
-            dotcard2.innerHTML = `
-            <p>
-                Me segue no 
-                <strong style="color: rgb(50, 124, 253);">Twitter</strong>
-                tó ativo lá também.
-            </p>`;
-        };
-        
+        switch(index){
+            case 0 :
+                dotPai.style.transform = `translateX(-15px)`;
+                dotcard2.innerHTML = `
+                <p>
+                    Oi! me chama no <strong style="color: rgb(0, 255, 102);">Whatsapp</strong>
+                    vamos converssar.
+                </p>`;
+            break   
+            case 1 :
+                dotPai.style.transform = `translateX(93.5px)`;
+                dotcard2.innerHTML = `
+                <p>
+                    Me segue no 
+                    <strong style="color: rgb(255, 0, 128);">Instagram</strong>
+                    e me chama no direct.
+                </p>`;
+            break   
+            case 2 :
+                dotPai.style.transform = `translateX(203px)`;
+                dotcard2.innerHTML = `
+                <p>
+                    Passa lá no meu <strong style="color:  rgb(39, 48, 161);">Linkedin</strong>.
+                </p>`;
+            break   
+            case 3 :
+                dotPai.style.transform = `translateX(311px)`;
+                dotcard2.innerHTML = `
+                <p>
+                    Dá uma olhadinha lá no meu 
+                    <strong 
+                        style="
+                            color: black; 
+                            text-shadow:  1px 1px 0px #F7004E, -1px -1px 0px  #00EAE3;
+                        ">
+                        TikTok
+                    </strong>.
+                </p>`;
+            break    
+            case 4 :
+                dotPai.style.transform = `translateX(420px)`;
+                dotcard2.innerHTML = `
+                <p>
+                    Me segue no 
+                    <strong style="color: rgb(50, 124, 253);">Twitter</strong>
+                    tó ativo lá também.
+                </p>`;
+            break    
+        }  
     });
-
 });
 
 navLeft.addEventListener('click', () => showContentOne());
