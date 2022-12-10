dff6080c0eb3a41f2f86ba4ad84998312104dd7c
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-12-09 21:48:46 -0300

    update

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='content'>
+        <h1>Hello world </h1>
+        <ul>
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
