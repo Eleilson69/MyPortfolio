a3b6970518d0257a323447b3cef0363012904f26
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-12-14 10:51:36 -0300

    update

--- my-portfolio/src/components/Card/NavActionComponent/index.jsx
+++ my-portfolio/src/components/Card/NavActionComponent/index.jsx
@@ -1,21 +1,31 @@
 import { Component } from 'react';
-import './styles.css'
-import imgHeart from '../imgs/03-9.png'
-import imgCassete from '../imgs/cassete.png'
+import './styles.css';
+import imgHeart from '../imgs/03-9.png';
+import imgCassete from '../imgs/cassete.png';
 
 export class NavActionComponent extends Component {
     render() {
-        const actionFunc = () => {
-            console.log('.navAction-left')
+        const actionFunc1 = () => {
+            const elem1 = document.querySelector('.navAction-left');
+            const elem2 = document.querySelector('.navAction-right');
+            elem1.classList.add('backgroundOn')
+            elem2.classList.remove('backgroundOn')
+        }
+        const actionFunc2 = () => {
+            const elem1 = document.querySelector('.navAction-left');
+            const elem2 = document.querySelector('.navAction-right');
+            elem1.classList.remove('backgroundOn')
+            elem2.classList.add('backgroundOn')
         }
         return (
             <div className='navAction'>
-            <div onClick={actionFunc} className='navAction-left'>
+            <div onClick={actionFunc1} className='navAction-left backgroundOn'>
                 <img className='imgCassete' src={imgCassete} />
             </div>
-            <div onClick={actionFunc} className='navAction-right'>
+            <div onClick={actionFunc2} className='navAction-right'>
                 <img className='imgHeart animateRotate' src={imgHeart} />
             </div>
+                   
             </div>
         )
     }
